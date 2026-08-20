c55a9a4a3b1d7b0d31a7b1eee233fdb587490695
Matias Zahn <zahnmatias14@gmail.com>
2026-08-19 23:35:13 -0300

    Cambios de prueba

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,14 @@
+import Login from "./pages/Login";
+
 function App() {
-  return <div className="bg-red-500">Hola</div>;
+  return (
+    <div className="bg-black text-white">
+      <h1 className="text-5xl">Hola chicos como estan</h1>
+      <h3 className="text-amber-300 fo">ACA ANDAMOS</h3>
+
+      <Login />
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,8 @@
+function Login() {
+  const llamarBoton = () => {
+    console.log("HHOLA CHICOS");
+  };
+
+  return <button onClick={llamarBoton}>BOTON</button>;
+}
+export default Login;
